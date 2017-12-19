34863fcb48b79871785fcd161b9671eaa93f3425
ganesshkumar <rpganesshkumar@gmail.com>
2017-12-19 17:06:35 +0530

    bugfixes

--- src/main/java/com/ganesshkumar/afterburn/parser/Parser.java
+++ src/main/java/com/ganesshkumar/afterburn/parser/Parser.java
@@ -10,11 +10,21 @@ public class Parser {
     private Handler handler = new Handler();
 
     public void acceptIncoming(DataInputStream inputStream, BufferedWriter outputWriter) throws IOException {
+        System.err.println("Begin Request");
+        // Read the incoming request from stdin
         Request request = new Reader(inputStream).getRequest();
-        String responseBody = handler.function(request.getBody(), request.getHeaders().getMethod());
-        String response = new Response(200, responseBody, "text/plain").serialize();
 
-        outputWriter.write(response, 0, response.length());
+        // Call the function with the request body
+        System.err.println("request body: " + request.getBody());
+        String responseBody = handler.function(request.getBody(), request.getHeaders().getMethod());
+        System.err.println("response body: " + responseBody);
+
+        // Make response to be returned
+        String response = new Response("200 OK", responseBody, "text/plain").serialize();
+        // Write the response to stdout
+        outputWriter.write(response);
         outputWriter.flush();
+
+        System.err.println("End Request");
     }
 }
